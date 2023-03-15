bb336a0eb26cb89bbe181e5b7595f5d38084e892
Cesar Perez <cesarperezsampedro@gmail.com>
2023-03-15 17:44:45 +0100

    Terminados los servicios de peliculas y actores

--- src/app/peliculas.service.ts
+++ src/app/peliculas.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Pelicula } from './models/pelicula';
-import { Actor } from './models/actor';
 import { ActoresService } from './actores.service';
 import { Personaje } from './models/personaje';
 
@@ -9,12 +8,25 @@ import { Personaje } from './models/personaje';
 })
 export class PeliculasService {
   
-  loQueElVientoSeLlevo: Pelicula;
-  elPadrino: Pelicula;
-  
+  //loQueElVientoSeLlevo: Pelicula;
+  //elPadrino: Pelicula;
+  peliculas: Pelicula[]= [];
 
   constructor(private actores: ActoresService) { 
 
+    this.peliculas.push(new Pelicula("Lo que el viento se llevo", 238, 1939))
+    this.peliculas[0].addPersonaje(new Personaje(actores.clarkGabel, "Rhett Butler ", "visitante de Charleston"));   
+    this.peliculas[0].addPersonaje(new Personaje(actores.leslieHoward, "Ashley Wilkes", "hijo"));
+    this.peliculas[0].addPersonaje(new Personaje(actores.vivienLeigh, "Scarlett O'Hara", "hija"));
+    this.peliculas[0].addPersonaje(new Personaje(actores.oliviadehavillan, "Melanie Hamilton", "prima"));
+
+    this.peliculas.push(new Pelicula("El padrino", 177, 1972))
+    this.peliculas[1].addPersonaje(new Personaje(actores.marlonBrando, "Don Vito Corleone", "Padrino"));   
+    this.peliculas[1].addPersonaje(new Personaje(actores.alPacino, "Michael Corleone", "Hijo menor del padrino"));
+    this.peliculas[1].addPersonaje(new Personaje(actores.robertDubball, "Thomas Hagen", "Consejero"));
+    this.peliculas[1].addPersonaje(new Personaje(actores.jamesCaan, "Santino Corleone", "Hijo mayor del padrino"));
+
+    /*
     this.loQueElVientoSeLlevo= new Pelicula("Lo que el viento se llevo", 238, 1939);
     this.loQueElVientoSeLlevo.addPersonaje( new Personaje(actores.clarkGabel, "Rhett Butler ", "visitante de Charleston"));
     this.loQueElVientoSeLlevo.addPersonaje( new Personaje(actores.leslieHoward, "Ashley Wilkes", "hijo"));
@@ -22,10 +34,10 @@ export class PeliculasService {
     this.loQueElVientoSeLlevo.addPersonaje( new Personaje(actores.oliviadehavillan, "Melanie Hamilton", "prima"));
     
     this.elPadrino= new Pelicula("El padrino", 177, 1972);
-    this.loQueElVientoSeLlevo.addPersonaje( new Personaje(actores.marlonBrando, "Don Vito Corleone", "Padrino"));
-    this.loQueElVientoSeLlevo.addPersonaje( new Personaje(actores.alPacino, "Michael Corleone", "Hijo menor del padrino"));
-    this.loQueElVientoSeLlevo.addPersonaje( new Personaje(actores.robertDubball, "Thomas Hagen", "Consejero"));
-    this.loQueElVientoSeLlevo.addPersonaje( new Personaje(actores.jamesCaan, "Santino Corleone", "Hijo mayor del padrino"));
+    this.elPadrino.addPersonaje( new Personaje(actores.marlonBrando, "Don Vito Corleone", "Padrino"));
+    this.elPadrino.addPersonaje( new Personaje(actores.alPacino, "Michael Corleone", "Hijo menor del padrino"));
+    this.elPadrino.addPersonaje( new Personaje(actores.robertDubball, "Thomas Hagen", "Consejero"));
+    this.elPadrino.addPersonaje( new Personaje(actores.jamesCaan, "Santino Corleone", "Hijo mayor del padrino"));*/
     
     
   }
